af6286381ed4490976faf88ba598efe43259281a
superdarkhorse0930 <superdarkhorse0930@gmail.com>
2024-11-30 03:16:40 +0900

    Init

---
+++ README.md
@@ -0,0 +1,69 @@
+## RetouchML &mdash; Beautify faces on images using StyleGan2
+![Python 3.6](https://img.shields.io/badge/python-3.6-green.svg?style=plastic)
+![TensorFlow 1.15](https://img.shields.io/badge/tensorflow-1.15-green.svg?style=plastic)
+![cuDNN 10.0](https://img.shields.io/badge/cudnn-10.0-green.svg?style=plastic)
+![License CC BY-NC](https://img.shields.io/badge/license-CC_BY--NC-green.svg?style=plastic)
+
+Beautify images using StyleGan2.
+
+Faces on images are recognized, normalised and converted into the corrensponding StyleGan2 Vector.
+
+Afterwards, the vector will be optimized to "beautify" the face.
+
+The face is then stiched back into the original image.
+
+## Before/After examples:
+
+All after images were generated entirely without human input.
+
+![merkel-ba](docs/merkel-ba.png)
+
+`© Raimond Spekking / CC BY-SA 4.0​`
+
+![pexel-ba](docs/pexel-ba.png)
+
+![couco-ba](docs/couco-ba.png)
+
+`© Gage Skidmore / CC BY-SA 3.0`
+
+
+## Manual Editing
+
+Latent space representations allow for manual changes to the output image.
+
+![manual](docs/merkel-change.gif)
+
+## How does it work?
+
+![process](docs/process.png)
+
+### 1. Latent space encoding
+
+First, the image is encoded into latent space. A latent vector is found that closely represents out input image. The encoder used is based on [StyleGan2 Encoder](https://github.com/robertluxemburg/stylegan2encoder.git).
+
+
+
+### 2. Latent vector optimisation
+
+Now to the fun part. Using a neural network that judges attractiveness of latent vectors, that face is optimized by the neural network. 
+
+Using a gradient ascent approach, the latent vector is slightly changed in the direction of steepest ascent, meaning we "make our face pretty".
+
+We'll get a new latent vector that generates a "prettier" version of the original face.
+
+
+
+### 3. Generate output image
+
+The new latent vector is now converted back into an image by StyleGan2. The generated image is then stiched back into the original image.
+
+
+
+## Want to try it for yourself?
+
+Clone the Repo(it needs an Nvidia GPU with Cuda and Tensorflow) or use Google Colab. To get started follow Beautify.ipynb
+
+
+
+
+
---
+++ fit_faces.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import argparse
+from ffhq_dataset.face_replacement import face_replace
+import bz2
+from keras.utils import get_file
+from ffhq_dataset.landmarks_detector import LandmarksDetector
+from shutil import copyfile
+import tqdm
+
+LANDMARKS_MODEL_URL = 'http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2'
+
+def unpack_bz2(src_path):
+    data = bz2.BZ2File(src_path).read()
+    dst_path = src_path[:-4]
+    with open(dst_path, 'wb') as fp:
+        fp.write(data)
+    return dst_path
+
+def main():
+    """
+    Extracts and aligns all faces from images using DLib and a function from original FFHQ dataset preparation step
+    python align_images.py /raw_images /aligned_images
+    """
+    parser = argparse.ArgumentParser(description='Find latent representation of reference images using perceptual loss')
+    parser.add_argument('src_file', help='Image to replace faces on')
+    parser.add_argument('face_path', help='Location of the faces to be replaced')
+    parser.add_argument('mask_path', help='Location of the masks for the face')
+    parser.add_argument('face_landmarks_path', help='File locations of the Numpy Arrays of the face locations')
+    parser.add_argument('dst_file', help='Output file location')
+
+    args, other_args = parser.parse_known_args()
+
+    landmarks_model_path = unpack_bz2(get_file('shape_predictor_68_face_landmarks.dat.bz2', LANDMARKS_MODEL_URL, cache_subdir='temp'))
+    #landmarks_model_path = unpack_bz2('models/shape_predictor_68_face_landmarks.dat.bz2')
+    landmarks_detector = LandmarksDetector(landmarks_model_path)
+
+    #Copy file to new location to work on it
+    copyfile(args.src_file, args.dst_file)
+
+    #Replace every face that has a mask
+    for f in os.listdir(args.face_path):
+        if f.endswith(".png") and f.startswith(os.path.splitext(os.path.basename(args.src_file))[0]):
+            filename = os.path.splitext(f)[0]
+            for i, generated_face_landmarks in enumerate(landmarks_detector.get_landmarks(args.face_path + filename + ".png"), start=1):
+                face_replace(args.dst_file, args.face_path  + filename + ".png", args.mask_path + filename + ".png", args.face_landmarks_path + filename + ".npy", generated_face_landmarks, args.dst_file )
+    print("Done!")
+    
+if __name__ == "__main__":
+    main()
